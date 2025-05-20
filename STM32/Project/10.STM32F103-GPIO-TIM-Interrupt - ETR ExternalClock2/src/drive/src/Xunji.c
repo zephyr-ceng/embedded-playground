e3212ed7108b6229b1b77e8d1ec52e9e3c8e122a
zephyr <zephyr_zeng@126.com>
2025-05-20 18:58:36 +0800

    update TIM input capture

---
+++ STM32/Project/10.STM32F103-GPIO-TIM-Interrupt - ETR ExternalClock2/src/drive/src/Xunji.c
@@ -0,0 +1,107 @@
+#include "stm32f10x.h"
+#include "./drive/inc/MyDelay.h"
+
+int32_t Overflow_value = 0; // 溢出计数器
+
+/* void TIM1_ETR_ExternalClock2_Config()
+{
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
+
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_12;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    // 定时器基础配置
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+    TIM_TimeBaseStructure.TIM_Period        = 10;
+    TIM_TimeBaseStructure.TIM_Prescaler     = 0;
+    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1; // 滤波的分频， 主要用于输入捕获和编码器接口
+    TIM_TimeBaseStructure.TIM_CounterMode   = TIM_CounterMode_Up;
+    TIM_TimeBaseInit(TIM1, &TIM_TimeBaseStructure);
+
+    // 配置 ETR 为外部时钟2触发源，边沿触发
+    TIM_ETRClockMode2Config(
+        TIM1,
+        TIM_ExtTRGPSC_OFF,              // 不分频
+        TIM_ExtTRGPolarity_NonInverted, // 上升沿触发（可改为 Inverted）
+        0x0F                            // 滤波器（最大值，抗抖动）
+    );
+
+    // TIM_SetCounter(TIM1, 0);
+    TIM_Cmd(TIM1, ENABLE);
+} */
+
+void TIM1_ETR_ExternalClock2_Overflow_Config()
+{
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
+
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_12;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+
+    // 定时器基础配置
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+    TIM_TimeBaseStructure.TIM_Period        = 9; // ARR 
+    TIM_TimeBaseStructure.TIM_Prescaler     = 0; // PSC
+    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1; // 滤波的分频， 主要用于输入捕获和编码器接口
+    TIM_TimeBaseStructure.TIM_CounterMode   = TIM_CounterMode_Up;
+    TIM_TimeBaseInit(TIM1, &TIM_TimeBaseStructure);
+
+    // 配置 ETR 为外部时钟2触发源，边沿触发
+    TIM_ETRClockMode2Config(
+        TIM1,
+        TIM_ExtTRGPSC_OFF,              // 不分频
+        TIM_ExtTRGPolarity_NonInverted, // 上升沿触发（可改为 Inverted）
+        0x0F                            // 滤波器（最大值，抗抖动）
+    );
+
+    // 6. 配置NVIC
+    NVIC_InitTypeDef NVIC_InitStructure;
+    NVIC_InitStructure.NVIC_IRQChannel                   = TIM1_UP_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority        = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd                = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
+    // 只允许计数器溢出触发更新中断
+    TIM_UpdateRequestConfig(TIM1,TIM_UpdateSource_Global);
+    // 清除中断位，防止上次中断未清除
+    TIM_ClearITPendingBit(TIM1, TIM_IT_Update);
+    TIM_ClearFlag(TIM1,TIM_FLAG_Update | TIM_IT_Update);
+
+    TIM_ITConfig(TIM1,TIM_IT_Update,ENABLE);
+    
+    // 7. 启动定时器
+    TIM_Cmd(TIM1, ENABLE);
+}
+
+void TIM1_UP_IRQHandler(void)
+{
+    if (TIM_GetITStatus(TIM1, TIM_IT_Update) != RESET) {
+        Overflow_value++;                            // 溢出计数器加1
+        TIM_ClearITPendingBit(TIM1, TIM_IT_Update); // 清除中断标志
+    }
+
+}
+
+int64_t Get_Overflow_value(void)
+{
+/*     uint16_t cnt, of;
+    do
+    {
+        of = Overflow_value;
+        cnt = TIM_GetCounter;
+    } while (of != Overflow_value);
+    // 防止此时中断溢出
+    if (TIM_GetITStatus(TIM1,TIM_IT_Update) != RESET && cnt == 0)
+    {
+        of++;
+    }
+ */
+    return Overflow_value;
+}
---
+++ STM32/Project/13..STM32F103-GPIO-TIM-Interrupt - ExternalColck1-TIM3-IC Capture-ranging/src/drive/src/Ranging.c
@@ -0,0 +1,113 @@
+#include "stm32f10x.h"
+#include "./drive/inc/MyDelay.h"
+
+volatile uint32_t riseTime      = 0; // 上升沿时间
+volatile uint32_t fallTime      = 0; // 下降沿时间
+volatile uint16_t captureStatus = 0; // 测量状态
+volatile uint32_t distance = 0;
+
+void GPIO_Init_Config()
+{
+    // 1.IO初始化
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+    GPIO_InitTypeDef Gpio_initStructure;
+    Gpio_initStructure.GPIO_Pin   = GPIO_Pin_6;
+    Gpio_initStructure.GPIO_Mode  = GPIO_Mode_Out_PP; // 推挽输出
+    Gpio_initStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &Gpio_initStructure);
+
+    Gpio_initStructure.GPIO_Pin   = GPIO_Pin_7;
+    Gpio_initStructure.GPIO_Mode  = GPIO_Mode_IN_FLOATING; // 浮空输入
+    Gpio_initStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &Gpio_initStructure);
+}
+
+void TIM3_CH_Config()
+{
+    // 2.初始化中断
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;
+    TIM_TimeBaseInitStructure.TIM_Period    = 65535;
+    TIM_TimeBaseInitStructure.TIM_Prescaler = 72 - 1;
+    // TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;       // 整个时钟的分频模式
+    TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up; // 计数模式
+    TIM_TimeBaseInit(TIM3, &TIM_TimeBaseInitStructure);
+
+    // 3.输入捕获配置 通道1
+    TIM_ICInitTypeDef TIM_ICIinitStructure;
+    TIM_ICIinitStructure.TIM_Channel     = TIM_Channel_1;            // 通道设置
+    TIM_ICIinitStructure.TIM_ICPolarity  = TIM_ICPolarity_Rising;    // 上升沿检测
+    TIM_ICIinitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI; // 直接模式
+    TIM_ICIinitStructure.TIM_ICPrescaler = TIM_CKD_DIV1;             // 输入时钟信号的分频设置
+    TIM_ICIinitStructure.TIM_ICFilter    = 15;                       // 滤波器设置
+    TIM_ICInit(TIM3, &TIM_ICIinitStructure);
+
+    // 3.输入捕获配置 通道2
+    TIM_ICIinitStructure.TIM_Channel     = TIM_Channel_2;
+    TIM_ICIinitStructure.TIM_ICPolarity  = TIM_ICPolarity_Falling;     // 下降沿
+    TIM_ICIinitStructure.TIM_ICSelection = TIM_ICSelection_IndirectTI; // 间接模式
+    TIM_ICIinitStructure.TIM_ICPrescaler = TIM_CKD_DIV1;               // 分频设置
+    TIM_ICIinitStructure.TIM_ICFilter    = 15;                         // 滤波器设置
+    TIM_ICInit(TIM3, &TIM_ICIinitStructure);
+
+    // 4.NVIC初始化
+    NVIC_InitTypeDef NVIC_InitStructure;
+    NVIC_InitStructure.NVIC_IRQChannel                   = TIM3_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority        = 1;
+    NVIC_InitStructure.NVIC_IRQChannelCmd                = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
+    // 5.配置输入捕获映射
+    TIM_SelectInputTrigger(TIM3, TIM_TS_ITR1); // TIM3 的IT1为触发源
+    TIM_SelectSlaveMode(TIM3, TIM_SlaveMode_Reset);
+
+    // 6.清除TIM3 IT_CC1标志位
+    TIM_ClearITPendingBit(TIM3, TIM_IT_CC1 | TIM_IT_CC2);
+    TIM_ITConfig(TIM3, TIM_IT_CC1 | TIM_IT_CC2,ENABLE);
+    // 7.使能TIM3定时器
+    TIM_Cmd(TIM3, ENABLE);
+}
+
+void TIM3_IRQnHandler(void)
+{
+    if (TIM_GetITStatus(TIM3, TIM_IT_CC1) != RESET) {
+        TIM_ClearITPendingBit(TIM3, TIM_IT_CC1);
+        if (captureStatus == 0) {
+            riseTime      = TIM_GetCounter(TIM3);
+            captureStatus = 1;
+        }
+    }
+
+    if (TIM_GetITStatus(TIM3, TIM_IT_CC2) != RESET) {
+        TIM_ClearITPendingBit(TIM3, TIM_IT_CC1);
+        if (captureStatus == 1) {
+            fallTime      = TIM_GetCounter(TIM3);
+            if (fallTime > riseTime)
+            {
+                // 单位CM
+                distance = (fallTime - riseTime) * 0.034 / 2; 
+            }else{
+                distance = (0xffff - riseTime) *0.034 /2;
+            }
+            
+            captureStatus = 2;
+        }
+    }
+}
+
+// 超声波触发模块
+void Trigger_Ultrasonic(){
+    GPIO_SetBits(GPIOA,GPIO_Pin_6); // 发送一个高电平
+    Delay_us(10);
+    GPIO_ResetBits(GPIOA,GPIO_Pin_6);
+
+    captureStatus = 0;
+    TIM_SetCounter(TIM3,0);
+}
+
+int16_t Get_distance_value(){
+    return distance;
+}
