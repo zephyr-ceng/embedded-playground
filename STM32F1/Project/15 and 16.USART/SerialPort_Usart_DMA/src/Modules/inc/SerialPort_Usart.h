5087b978332bd5430316e8bc4ed71b3b5d600f33
zephyr <zephyr_zeng@126.com>
2025-08-01 17:43:20 +0800

    添加USART DMA驱动及相关配置文件

---
+++ STM32F1/Project/15 and 16.USART/SerialPort_Usart_DMA/src/Modules/inc/SerialPort_Usart.h
@@ -0,0 +1,9 @@
+#ifndef _SerialPort_Usart_H
+#define _SerialPort_Usart_H
+#include "stm32f10x.h"
+/*******************************  *******************************/
+void USART_DMA_Init(USART_TypeDef *USARTx, uint32_t BaudRate);
+void Get_ReviceData(uint16_t *Buffer);
+void Set_SendData(USART_TypeDef *USARTx,uint16_t *Buffer, uint16_t Length);
+
+#endif // !_SerialPort_Usart_H
---
+++ STM32F1/Project/15 and 16.USART/SerialPort_Usart_DMA/src/main.c
@@ -0,0 +1,30 @@
+/****************
+ *@description: Usart Driver for Stm32F1 about DMA
+ *@author: zephyr
+ *@date: 2025-07-30 10:50:30
+ *@version: V1.0.0
+****************/
+
+#include "./BSP_Drive/inc/MyDelay.h"
+#include "./Modules/inc/SerialPort_Usart.h"
+#include "string.h"
+
+char msg[] = "Hello, USART with DMA!\r\n";
+uint16_t ReviceData[256]; // Buffer to store received data
+
+int main(void)
+{
+    USART_DMA_Init(USART1, 115200); // Initialize USART1 with DMA
+    Get_ReviceData(ReviceData); // Get received data, 接收的数据会自动通过DMA存储到ReviceData中，不需要手动读取
+    while (1)
+    {
+/*         if (ReviceData)
+        {
+            // 在OLED或其他显示设备上显示接收到的数据
+        } */
+        Set_SendData(USART1, (uint16_t *)msg, strlen(msg)); // Send data via USART1
+        
+    }
+    
+    return 0;
+}
